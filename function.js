1f4a06da4dafdad46de6f6a56be12d1a1d49f561
HappyKumamoto <67646107+HappyKumamoto@users.noreply.github.com>
2021-05-22 03:26:25 +0900

    Update function.js

--- function.js
+++ function.js
@@ -2,18 +2,18 @@
 let hisBirthTime = new Date( 1897, 5, 21, 0, 0 );
 
 function updateP() {
-  let now = new Date();
-  let seconds = (now.getTime()-hisBirthTime.getTime())/1000;
-  let message = `生まれてから${Math.floor(seconds)}秒経過しています。`;
-  document.getElementById('birth-time').innerText = message;
-   }
+
+   let now = new Date();
   
-  function updateDiv() {
-  	let now = new Date();
-    let days = (now.getTime()-hisBirthTime.getTime())/(1000*60*60*24);
-    let bun = `生まれてから${Math.floor(days)}日目です。`;
-    document.getElementById('birth-days').innerText= bun;
+   //生まれてからの秒数
+   let seconds = (now.getTime()-hisBirthTime.getTime())/1000;
+   let message = `生まれてから${Math.floor(seconds)}秒経過しています。`;
+   document.getElementById('birth-time').innerText = message;
+   
+  //生まれてからの日数
+   let days = (now.getTime()-hisBirthTime.getTime())/(1000*60*60*24);
+   let bun = `生まれてから${Math.floor(days)}日目です。`;
+   document.getElementById('birth-days').innerText= bun;
   }
 
   setInterval(updateP,1000);
-  setInterval(updateDiv,1000);
